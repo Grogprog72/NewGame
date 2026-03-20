13d3b224d98612dd7f590558fb53f9cae32cb15f
Grogprog72 <NoskovGP@yandex.ru>
2026-03-20 17:03:06 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from typing import List
+possible_move = ['stone', 'scissors', 'paper']
+game_score = {'total_move': 0,
+              'player_score': 0,
+              'bot_score': 0}
+
+def bot_turn(possible_move: List[str]) -> str:
+    pass
+
+def player_turn() -> str:
+        pass
+
+def move_result(player_move: str, bot_move: str) -> None:
+        pass
